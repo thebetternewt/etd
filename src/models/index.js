c1de8b1b01e6b88db0a13bbe0a312c3e589457d4
Chris Eady <chris.eady1@gmail.com>
2018-11-26 12:10:01 -0600

    Try different sequelize args

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,8 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  console.log(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(config.use_env_variable);
 } else {
   sequelize = new Sequelize(
     config.database,
