c9f58a20520c6720993e949612fe6c24a90d448b
Chris Eady <chris.eady1@gmail.com>
2018-11-26 11:50:25 -0600

    Add procfile and update production config

--- config/database.js
+++ config/database.js
@@ -17,10 +17,10 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // dialect: process.env.DB_DIALECT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
 };
