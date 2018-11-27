04c5377803364c0313629104a80b74fb03fcba0c
Chris Eady <chris.eady1@gmail.com>
2018-11-26 22:26:54 -0600

    Fixy proxy config

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(proxy('/', { target: 'http://localhost:4000/' }));
+  // app.use(proxy / ('/', { target: 'http://localhost:4000/' }));
+  app.use(proxy('/graphql', { target: 'http://localhost:4000/graphql' }));
   app.use(proxy('/api', { target: 'http://localhost:4000/' }));
   app.use(proxy('/voyager', { target: 'http://localhost:4000/voyager' }));
 };
