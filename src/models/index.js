77c617c0dae98c66107ab4b762b57ce5d198305f
Chris Eady <chris.eady1@gmail.com>
2018-11-26 21:08:51 -0600

    More sequelize edits

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable[env], {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialect: config.dialect,
   });
 } else {
